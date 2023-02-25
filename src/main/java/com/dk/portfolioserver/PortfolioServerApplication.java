a806e2b06a3471f61721441861712fdf4f44947c
Dominik Kondrot <dominik.kondrot@gmail.com>
2023-02-25 21:08:12 +0100

    Initial setup

---
+++ src/main/java/com/dk/portfolioserver/PortfolioServerApplication.java
@@ -0,0 +1,13 @@
+package com.dk.portfolioserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dk/portfolioserver/PortfolioServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dk.portfolioserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
